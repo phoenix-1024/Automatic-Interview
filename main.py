2602cd75847472a814dc6532ce7d91f0f8adc95c
Sudesh <sudeshrai1024@gmail.com>
2024-03-16 03:31:20 +0530

    added endpoint for generating questions using job discription

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# here we will put the fast api code
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.q_and_a.QA import make_questions_form_jd
+from api_input_schema import jd_input
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("generate_questions/")
+def generate_questions(jd: jd_input):
+    return make_questions_form_jd(jd.job_discription)
+    
